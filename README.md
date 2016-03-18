c327ccd3f4ef7be412860bedf44167ebedea637c
Tyler Long <tyler4long@gmail.com>
2016-03-18 17:11:42 +0800

    First working version

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # rc-commander
 
 RingCentral in your terminal
+
+
+## Quick start
+
+### Step 1
+
+```
+npm install rc-commander
+```
+
+### Step 2
+
+Create `~/.rc-commander.json`, with the following content:
+
+```
+{
+  "server": "https://platform.devtest.ringcentral.com", // or https://platform.ringcentral.com for production
+  "appKey": "your app key",
+  "appSecret": "your app secret",
+  "username": "username is phone number",
+  "password": "password"
+}
+```
+
+### Step 3
+
+Enjoy RingCentral in your terminal:
+
+```
+rc sms 147258369 "hello world"
+
+```
--- rc.js
+++ rc.js
@@ -1,1 +1,27 @@
-console.log('hello world');
+var SDK = require('ringcentral');
+var fs = require('fs');
+
+var config = JSON.parse(fs.readFileSync(process.env['HOME'] + '/.rc-commander.json', 'utf8'));
+var rcsdk = new SDK({ server: config.server,
+  appKey: config.appKey,
+  appSecret: config.appSecret });
+
+var platform = rcsdk.platform();
+
+rcsdk.platform()
+    .login({
+        username: config.username, // phone number in full format
+        extension: '', // leave blank if direct number is used
+        password: config.password
+    })
+    .then(function(response) {
+      rcsdk.platform().post('/account/~/extension/~/sms', {
+        text: 'hello world',
+        from: { phoneNumber: '17322764403' },
+        to: [{ phoneNumber: '147258369' }],
+      }).then(function() { console.log('sms sent'); })
+        .catch(function(e) { console.log(e); });
+    })
+    .catch(function(e) {
+        console.log(e.message  || 'Server cannot authorize user');
+    });
