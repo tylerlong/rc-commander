2b9cf9e5e3187e39bd16815da37e5519d0582e2b
Tyler Long <tyler4long@gmail.com>
2016-03-21 09:58:52 +0800

    Use commander

--- rc-sms.js
+++ rc-sms.js
@@ -1,8 +1,4 @@
 var program = require('commander');
-var nunjucks = require('nunjucks');
-var path = require('path');
-var configuration = require('./configuration');
-var file = require('./file');
 
 
 program
@@ -10,3 +6,35 @@ program
   .option('-n, --number <number>', 'send to which number')
   .option('-t, --text <text>', 'text of the sms')
   .parse(process.argv);
+
+
+console.log("sending sms...");
+
+
+var SDK = require('ringcentral');
+var fs = require('fs');
+
+var config = JSON.parse(fs.readFileSync(process.env['HOME'] + '/.rc-commander.json', 'utf8'));
+var rcsdk = new SDK({ server: config.server,
+  appKey: config.appKey,
+  appSecret: config.appSecret });
+
+var platform = rcsdk.platform();
+
+platform
+    .login({
+        username: config.username, // phone number in full format
+        extension: '', // leave blank if direct number is used
+        password: config.password
+    })
+    .then(function(response) {
+      rcsdk.platform().post('/account/~/extension/~/sms', {
+        text: program.text,
+        from: { phoneNumber: '17322764403' },
+        to: [{ phoneNumber: program.number }],
+      }).then(function() { console.log('sms sent'); })
+        .catch(function(e) { console.log(e); });
+    })
+    .catch(function(e) {
+        console.log(e.message  || 'Server cannot authorize user');
+    });
--- rc.js
+++ rc.js
@@ -1,27 +1,10 @@
-var SDK = require('ringcentral');
-var fs = require('fs');
+#! /usr/bin/env node
 
-var config = JSON.parse(fs.readFileSync(process.env['HOME'] + '/.rc-commander.json', 'utf8'));
-var rcsdk = new SDK({ server: config.server,
-  appKey: config.appKey,
-  appSecret: config.appSecret });
 
-var platform = rcsdk.platform();
+var program = require('commander');
 
-rcsdk.platform()
-    .login({
-        username: config.username, // phone number in full format
-        extension: '', // leave blank if direct number is used
-        password: config.password
-    })
-    .then(function(response) {
-      rcsdk.platform().post('/account/~/extension/~/sms', {
-        text: 'hello world',
-        from: { phoneNumber: '17322764403' },
-        to: [{ phoneNumber: '147258369' }],
-      }).then(function() { console.log('sms sent'); })
-        .catch(function(e) { console.log(e); });
-    })
-    .catch(function(e) {
-        console.log(e.message  || 'Server cannot authorize user');
-    });
+
+program
+  .version(require('./package.json').version)
+  .command('sms', 'send sms')
+  .parse(process.argv);
